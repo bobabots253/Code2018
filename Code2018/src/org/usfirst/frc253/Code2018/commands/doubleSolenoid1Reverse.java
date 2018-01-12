5eef2ee611a45c0d556f568870d7c0af50c21183
mhs <mhs@MHS-Science2-13>
2018-01-11 16:45:55 -0800

    SO in this code branch, we have made the double solenoids and compressor. And put them into a command, making them work.

---
+++ Code2018/src/org/usfirst/frc253/Code2018/commands/doubleSolenoid1Reverse.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc253.Code2018.commands;
+
+import org.usfirst.frc253.Code2018.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class doubleSolenoid1Reverse extends Command {
+
+    public doubleSolenoid1Reverse() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.pneumatics.doubleSolenoid1Reverse();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
