3c08d888cb378fb3197701905fe7d7550b85bd2f
mhs <mhs@MHS-Science2-13>
2018-01-20 11:29:21 -0800

    made cycle codes

---
+++ Code2018/src/org/usfirst/frc253/Code2018/commands/cyclePortalToSwitch2.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc253.Code2018.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class cyclePortalToSwitch2 extends CommandGroup {
+
+    public cyclePortalToSwitch2() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    	for(int i = 0; i <= 4; i++){
+    		//move to switch area
+    		addSequential(new SwitchScore());
+    		//move back to different portal
+    		addSequential(new grab());
+    		//repeat
+    	}    	
+    }
+}
