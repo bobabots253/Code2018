628ddd88333bfc085d39c7306896010e112c7062
DESKTOP-GF8RGT1\aaron <aaronyu330@gmail.com>
2018-07-22 23:50:06 -0700

    whups forgot to initialize drivetrain subsystem

--- src/main/kotlin/frc/team253/robot/Robot.kt
+++ src/main/kotlin/frc/team253/robot/Robot.kt
@@ -2,11 +2,13 @@ package frc.team253.robot
 
 import edu.wpi.first.wpilibj.IterativeRobot
 import edu.wpi.first.wpilibj.command.Scheduler
+import frc.team253.robot.drivetrain.DrivetrainSubsystem
 
 class Robot : IterativeRobot(){
 
     init{
         OI
+        DrivetrainSubsystem
     }
 
     override fun robotInit(){
